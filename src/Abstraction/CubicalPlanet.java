de22dcf7380b09c335f6b72f1fc8e797d805fd13
Acer <Acer@192.168.5.6>
2020-01-03 21:18:03 +0530

    ABSTRACTION :  In this application using the abstraction to find the shapes of the cone,cube.

---
+++ src/Abstraction/CubicalPlanet.java
@@ -0,0 +1,22 @@
+package Abstraction;
+public class CubicalPlanet extends Planet {
+private float side;
+public CubicalPlanet(String planetName,float side) {
+super(planetName);
+this.side = side;
+}
+
+public float getSide() {
+return side;
+}
+
+public void setSide(float side) {
+this.side = side;
+}
+
+public void display() {
+float area = this.getSide()*this.getSide()*this.getSide();
+System.out.printf("%ekm3", area);
+}
+
+}
---
+++ src/Abstraction/Planet.java
@@ -0,0 +1,16 @@
+package Abstraction;
+
+public abstract class Planet {
+protected String planetName;
+public Planet(String planetName) {
+super();
+this.planetName = planetName;
+}
+public String getPlanetName() {
+return planetName;
+}
+public void setPlanetName(String planetName) {
+this.planetName = planetName;
+}
+
+}
